024a1ee2985810051a2e63bcbf6b8bbf6fb4ed94
sunwhdgus <hedgehog1224@gmail.com>
2026-01-19 18:50:53 +0900

    feat: DTO 추가 및 롬복 적용, 테스트 코드 작성

---
+++ src/main/java/com/backend/study/hellospring/web/HelloController.java
@@ -0,0 +1,20 @@
+package com.backend.study.hellospring.web;
+
+import com.backend.study.hellospring.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return ":Hello World!";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}
